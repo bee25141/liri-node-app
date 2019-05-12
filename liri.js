5d90761f9e9ea7d4195d5c28e5c41ee60855399f
bee25141 <bee25141@gmail.com>
2019-05-12 15:49:10 -0500

    able to run axios get request for movies queries

--- liri.js
+++ liri.js
@@ -1,4 +1,17 @@
 require("dotenv").config();
-var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
-console.log(spotify);
+const keys = require("./keys.js");
+const axios = require("axios");
+// const spotify = new Spotify(keys.spotify);
+var userOption = process.argv[2];
+var userInput = process.argv.slice(3);
+
+if (userOption = "movie-this"){
+    if (userInput === undefined){
+        console.log("undefined");
+    }else{
+        axios.get("https://www.omdbapi.com/?t=" + userInput + "&apikey=trilogy")
+        .then(function(response){
+            console.log(JSON.stringify(response.data));
+        });
+    }
+}
