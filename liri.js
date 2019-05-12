f7de532dd5d6051ee4535e8e61c7f0e2daf76218
bee25141 <bee25141@gmail.com>
2019-05-12 16:09:27 -0500

    updated movie response display, need to incorporate rotten tomatoes rating

--- liri.js
+++ liri.js
@@ -10,8 +10,15 @@ if (userOption = "movie-this"){
         console.log("undefined");
     }else{
         axios.get("https://www.omdbapi.com/?t=" + userInput + "&apikey=trilogy")
-        .then(function(response){
-            console.log(JSON.stringify(response.data));
+        .then(response => {
+            console.log(JSON.stringify("Title: " + response.data.Title));
+            console.log(JSON.stringify("Year: " + response.data.Year));
+            console.log(JSON.stringify("Actors: " + response.data.Actors));
+            console.log(JSON.stringify("Plot: " + response.data.Plot));
+            console.log(JSON.stringify("Language: " + response.data.Language));
+            console.log(JSON.stringify("Country Produced: " + response.data.Country));
+            console.log(JSON.stringify("IMDB Rating: " + response.data.imdbRating));
+
         });
     }
 }
