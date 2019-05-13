aa68a0a44603e90a7ac307a5e100e308a20fd6bd
bee25141 <bee25141@gmail.com>
2019-05-12 19:55:29 -0500

    added get request to bandsintown api to get upcoming concert info

--- liri.js
+++ liri.js
@@ -1,24 +1,48 @@
 require("dotenv").config();
 const keys = require("./keys.js");
 const axios = require("axios");
-// const spotify = new Spotify(keys.spotify);
+const moment = require('moment');
+const Spotify = require('node-spotify-api');
+const spotify = new Spotify(keys.spotify);
 var userOption = process.argv[2];
 var userInput = process.argv.slice(3);
 
-if (userOption = "movie-this"){
-    if (userInput === undefined){
+//Function that gathers movie info from the OMDB api
+if (userOption === "movie-this") {
+    if (userInput === undefined) {
+        console.log("undefined");
+    } else {
+        axios.get("https://www.omdbapi.com/?t=" + userInput + "&tomatoes=true&apikey=trilogy")
+            .then(response => {
+                console.log(JSON.stringify("Title: " + response.data.Title));
+                console.log(JSON.stringify("Year: " + response.data.Year));
+                console.log(JSON.stringify("Actors: " + response.data.Actors));
+                console.log(JSON.stringify("Plot: " + response.data.Plot));
+                console.log(JSON.stringify("Language: " + response.data.Language));
+                console.log(JSON.stringify("Country Produced: " + response.data.Country));
+                console.log(JSON.stringify("Rotten Tomatoes Rating: " + response.data.tomatoeRating));
+                console.log(JSON.stringify("IMDB Rating: " + response.data.imdbRating));
+                console.log(response);
+
+            });
+    }
+}
+
+//Function for getting concert info from the Bands in Town API
+if (userOption === "concert-this") {
+    if(userInput === undefined){
         console.log("undefined");
     }else{
-        axios.get("https://www.omdbapi.com/?t=" + userInput + "&apikey=trilogy")
+        axios.get("https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp")
         .then(response => {
-            console.log(JSON.stringify("Title: " + response.data.Title));
-            console.log(JSON.stringify("Year: " + response.data.Year));
-            console.log(JSON.stringify("Actors: " + response.data.Actors));
-            console.log(JSON.stringify("Plot: " + response.data.Plot));
-            console.log(JSON.stringify("Language: " + response.data.Language));
-            console.log(JSON.stringify("Country Produced: " + response.data.Country));
-            console.log(JSON.stringify("IMDB Rating: " + response.data.imdbRating));
-
-        });
+            // console.log(response.data);
+            for (i=0; i<response.data.length; i++){
+                console.log(response.data[i].venue.name);
+                console.log(response.data[i].venue.city);
+                var dateTimeConverted = response.data[i].datetime;
+                dateTimeConverted = moment(dateTimeConverted).format("MM/DD/YYYY");
+                console.log(dateTimeConverted);
+            }
+        })
     }
 }
