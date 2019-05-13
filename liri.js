99fa4356c062f778d6f69396eaeb717052d4749e
bee25141 <bee25141@gmail.com>
2019-05-12 21:46:06 -0500

    working on gettingrequets from the spotify api

--- liri.js
+++ liri.js
@@ -7,6 +7,7 @@ const spotify = new Spotify(keys.spotify);
 var userOption = process.argv[2];
 var userInput = process.argv.slice(3);
 
+
 //Function that gathers movie info from the OMDB api
 if (userOption === "movie-this") {
     if (userInput === undefined) {
@@ -22,13 +23,13 @@ if (userOption === "movie-this") {
                 console.log(JSON.stringify("Country Produced: " + response.data.Country));
                 console.log(JSON.stringify("Rotten Tomatoes Rating: " + response.data.tomatoeRating));
                 console.log(JSON.stringify("IMDB Rating: " + response.data.imdbRating));
-                console.log(response);
+                console.log(response); 
 
             });
     }
 }
 
-//Function for getting concert info from the Bands in Town API
+//Getting concert info from the Bands in Town API
 if (userOption === "concert-this") {
     if(userInput === undefined){
         console.log("undefined");
@@ -44,5 +45,21 @@ if (userOption === "concert-this") {
                 console.log(dateTimeConverted);
             }
         })
+    }
+}
+
+//Getting the song info from the Spotify API
+if(userOption === "spotify-this-song"){
+    if(userInput === undefined){
+        console.log(undefined);
+    }else {
+        spotify.getClientCredentialsGrant()
+        .then(data => {
+            spotify.setAccessToken(data.body['access_token']);
+            console.log(spotify);
+        })
+        // .then(response => {
+        //     console.log(response);
+        // })
     }
 }
