d38d0f712adcea619c84a3743e4ecb258b9353fb
bee25141 <bee25141@gmail.com>
2019-05-13 20:08:46 -0500

    functional soundcloud api get request

--- liri.js
+++ liri.js
@@ -53,13 +53,15 @@ if(userOption === "spotify-this-song"){
     if(userInput === undefined){
         console.log(undefined);
     }else {
-        spotify.getClientCredentialsGrant()
-        .then(data => {
-            spotify.setAccessToken(data.body['access_token']);
-            console.log(spotify);
-        })
-        // .then(response => {
-        //     console.log(response);
-        // })
+        spotify.search({type: 'track', query: userInput})
+        .then(response => {
+            // console.log(response.tracks.items);
+            for (i=0; i < 4; i++){
+                console.log("Artist: " + JSON.stringify(response.tracks.items[i].artists[0].name));
+                console.log("Track Name: " + response.tracks.items[i].name);
+                console.log("Album: " + response.tracks.items[i].album.name);
+                console.log("Song Preview: " + response.tracks.items[i].preview_url);
+            }
+        });
     }
 }
