4a37825ad2f951886ea7ffe804d3c5259b28b466
bee25141 <bee25141@gmail.com>
2019-05-15 08:06:40 -0500

    finished up 'do whatever it says' command

--- liri.js
+++ liri.js
@@ -24,57 +24,100 @@ if (userOption === "movie-this") {
                 console.log(JSON.stringify("Country Produced: " + response.data.Country));
                 console.log(JSON.stringify("Rotten Tomatoes Rating: " + response.data.tomatoeRating));
                 console.log(JSON.stringify("IMDB Rating: " + response.data.imdbRating));
-                console.log(response); 
+                console.log(response);
             });
-    }
-}
+    };
+};
 
 //Getting concert info from the Bands in Town API
 if (userOption === "concert-this") {
-    if(userInput === undefined){
+    if (userInput === undefined) {
         console.log("undefined");
-    }else{
+    } else {
         axios.get("https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp")
-        .then(response => {
-            // console.log(response.data);
-            for (i=0; i<response.data.length; i++){
-                console.log(response.data[i].venue.name);
-                console.log(response.data[i].venue.city);
-                var dateTimeConverted = response.data[i].datetime;
-                dateTimeConverted = moment(dateTimeConverted).format("MM/DD/YYYY");
-                console.log(dateTimeConverted);
-            }
-        })
-    }
-}
+            .then(response => {
+                // console.log(response.data);
+                for (i = 0; i < response.data.length; i++) {
+                    console.log(response.data[i].venue.name);
+                    console.log(response.data[i].venue.city);
+                    var dateTimeConverted = response.data[i].datetime;
+                    dateTimeConverted = moment(dateTimeConverted).format("MM/DD/YYYY");
+                    console.log(dateTimeConverted);
+                };
+            })
+    };
+};
 
 //Getting the song info from the Spotify API
-if(userOption === "spotify-this-song"){
-    if(userInput === undefined){
+if (userOption === "spotify-this-song") {
+    if (userInput === undefined) {
         console.log(undefined);
-    }else {
-        spotify.search({type: 'track', query: userInput})
-        .then(response => {
-            // console.log(response.tracks.items);
-            for (i=0; i < 4; i++){
-                console.log("Artist: " + JSON.stringify(response.tracks.items[i].artists[0].name));
-                console.log("Track Name: " + response.tracks.items[i].name);
-                console.log("Album: " + response.tracks.items[i].album.name);
-                console.log("Song Preview: " + response.tracks.items[i].preview_url);
-            }
-        }); 
-    }
-}
+    } else {
+        spotify.search({
+                type: 'track',
+                query: userInput
+            })
+            .then(response => {
+                // console.log(response.tracks.items);
+                for (i = 0; i < 4; i++) {
+                    console.log("Artist: " + JSON.stringify(response.tracks.items[i].artists[0].name));
+                    console.log("Track Name: " + response.tracks.items[i].name);
+                    console.log("Album: " + response.tracks.items[i].album.name);
+                    console.log("Song Preview: " + response.tracks.items[i].preview_url);
+                };
+            });
+    };
+};
 
 //takes instruction from txt file random.txt and performs commands
-if(userOption === "do-what-it-says"){
-fs.readFile("random.txt", "utf8", function(error, data) {
-
-    if (error) {
-      return console.log(error);
-    }
-    console.log(data);
-    // var dataArr = data.split(",");
-    // console.log(dataArr);
-  });
-}
+if (userOption === "do-what-it-says") {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        } else {
+            console.log(data);
+            let dataArray = data.split(",");
+            let userInput = dataArray.slice(1);
+            if (dataArray[0] === "spotify-this-song") {
+                spotify.search({
+                        type: 'track',
+                        query: userInput
+                    })
+                    .then(response => {
+                        // console.log(response.tracks.items);
+                        for (i = 0; i < 4; i++) {
+                            console.log("Artist: " + JSON.stringify(response.tracks.items[i].artists[0].name));
+                            console.log("Track Name: " + response.tracks.items[i].name);
+                            console.log("Album: " + response.tracks.items[i].album.name);
+                            console.log("Song Preview: " + response.tracks.items[i].preview_url);
+                        }
+                    });
+            } else if (dataArray[0] === "movie-this") {
+                axios.get("https://www.omdbapi.com/?t=" + userInput + "&tomatoes=true&apikey=trilogy")
+                    .then(response => {
+                        console.log(JSON.stringify("Title: " + response.data.Title));
+                        console.log(JSON.stringify("Year: " + response.data.Year));
+                        console.log(JSON.stringify("Actors: " + response.data.Actors));
+                        console.log(JSON.stringify("Plot: " + response.data.Plot));
+                        console.log(JSON.stringify("Language: " + response.data.Language));
+                        console.log(JSON.stringify("Country Produced: " + response.data.Country));
+                        console.log(JSON.stringify("Rotten Tomatoes Rating: " + response.data.tomatoeRating));
+                        console.log(JSON.stringify("IMDB Rating: " + response.data.imdbRating));
+                        console.log(response);
+                    });
+            } else if (dataArray[0] === "concert-this") {
+                axios.get("https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp")
+                    .then(response => {
+                        // console.log(response.data);
+                        for (i = 0; i < response.data.length; i++) {
+                            console.log(response.data[i].venue.name);
+                            console.log(response.data[i].venue.city);
+                            var dateTimeConverted = response.data[i].datetime;
+                            dateTimeConverted = moment(dateTimeConverted).format("MM/DD/YYYY");
+                            console.log(dateTimeConverted);
+                        };
+                    });
+            };
+        };
+    });
+};
